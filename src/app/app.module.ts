b243261af523646848baccb9b3169a3fcfc86124
Manuel <m.a.c.p90@gmail.com>
2017-11-04 12:12:56 -0400

    my two ways component detec something wrong with the syntax

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyPropertyBindingComponent } from './my-property-binding/my-property-binding.component';
+import { MyEventBindingComponent } from './my-event-binding/my-event-binding.component';
+import { MyMixedBindingComponent } from './my-mixed-binding/my-mixed-binding.component';
+import { MyTwoWayBindingComponent } from './my-two-way-binding/my-two-way-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyPropertyBindingComponent,
+    MyEventBindingComponent,
+    MyMixedBindingComponent,
+    MyTwoWayBindingComponent
   ],
   imports: [
     BrowserModule
